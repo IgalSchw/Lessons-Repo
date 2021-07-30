ea9c0100ce5ba940b65855006d95dbeaac9f73a5
Igal <IgalShwartz@gmail.com>
2021-07-31 02:08:24 +0300

    DZ - 3
    Arrays and Loops

---
+++ Lessons/Lessons/Lesson_4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Lessons
+{
+     // Lesson 4 - Methods
+    /*
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("START!");
+
+            PrtMsg("Igal");
+
+            Console.WriteLine("END!");
+        }
+
+        static void PrtMsg(string msg)
+        {
+            Console.WriteLine($"Hello {msg}");
+        }
+
+        static int CalcNumbers(int a, int b)
+        {
+            return a + b;
+        }
+
+
+        // Kortezh method
+        static (string name,int age, string surname) AskUser() 
+        {
+            string name = string.Empty;
+            int age = 0;
+            string surname = string.Empty;
+
+            return (name, age, surname);
+        }
+
+    }
+
+    */
+
+}
